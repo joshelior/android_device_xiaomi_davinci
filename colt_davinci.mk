8b0ccae9d89ac57764b4aee91c03655e675bf30c
joshelior <nwokochajoshua@gmail.com>
2020-10-15 19:28:24 +0000

    It's colt time

--- colt_davinci.mk
+++ colt_davinci.mk
@@ -21,10 +21,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from davinci device
 $(call inherit-product, device/xiaomi/davinci/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Colt stuff.
+TARGET_BOOT_ANIMATION_RES := 1080x2340
+$(call inherit-product, vendor/colt/config/common_full_phone.mk)
+COLT_DEVICE_MAINTAINER := joshelior
 
-PRODUCT_NAME := lineage_davinci
+TARGET_HAS_FOD := true
+
+PRODUCT_NAME := colt_davinci
 PRODUCT_DEVICE := davinci
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi 9T
